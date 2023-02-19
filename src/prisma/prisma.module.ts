c3ea46a29b40799353418e11ef66454601068d35
Rishat Agzamov <rishat.se@gmail.com>
2023-02-19 13:24:11 -0500

    fix: refine prisma module

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -2,12 +2,6 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
 @Module({
-  // imports: [
-  //     forwardRef(() => FavoritesModule),
-  //     forwardRef(() => AlbumsModule),
-  //     forwardRef(() => ArtistsModule),
-  //   ],
-  //   controllers: [TracksController],
   providers: [PrismaService],
   exports: [PrismaService],
 })
